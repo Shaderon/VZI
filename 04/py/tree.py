d49c2fa9309825546454ac0b3751ea42d9e65b73
uai <uai@hotmail.cz>
2016-10-26 23:38:04 +0200

    update tree

--- 04/py/tree.py
+++ 04/py/tree.py
@@ -35,20 +35,33 @@ class Node:
 class BTree:
 
     def __init__(self):
-        pass
+        # pass
+        self.root = None
 
-    def insert(self, data, parent=None):
-        node = Node(data)
+    def getRoot(self):
+        return self.root
+
+    def insert(self, data):
+        self.root = self.insert_data(self.root, data)
+
+        return self.root
+
+    def insert_data(self, parent, data):
 
         if parent == None:
+            node = Node(data)
             return node
-        else:
-            if data < parent.getData():
-                parent.setLeft(node)
-            else:
-                parent.setRight(node)
 
-        return node
+        if data == parent.getData():
+            return parent
+
+        if data < parent.getData():
+            parent.setLeft(self.insert_data(parent.getLeft(), data))
+        else:
+            parent.setRight(self.insert_data(parent.getRight(), data))
+
+        return parent
+
 
     # Prochazeni stromu a hledani
     # Mame dve metody DFS (do hloubky) a BFS (do sirky).
@@ -84,9 +97,10 @@ class BTree:
 
     # DFS traversing, non recursive
     # https://en.wikipedia.org/wiki/Depth-first_search
-    def dfs_traverse(self, root):
+    # def dfs_traverse(self, root):
+    def dfs_traverse(self):
         lifo = stack.Stack()
-        lifo.push(root)
+        lifo.push(self.root)
 
         while not lifo.isEmpty():
             node = lifo.pop()
@@ -98,9 +112,10 @@ class BTree:
 
     # BFS traversing
     # https://en.wikipedia.org/wiki/Breadth-first_search
-    def bfs_traverse(self, node):
+    def bfs_traverse(self):
+    # def bfs_traverse(self, node):
         fifo = queue.Queue()
-        fifo.push(node)
+        fifo.push(self.root)
 
         while not fifo.isEmpty():
             n = fifo.pop()
@@ -111,49 +126,57 @@ class BTree:
                 fifo.push(n.getRight())
 
     # breath first search
-    def search_bfs(self, root, nodeToFind):
+    def search_bfs(self, data):
         '''
             Prochazime strom do sirky a hledame pozadovany uzel.
             Nerekurzivni funkce za pouziti fronty.
         '''
         fifo = queue.Queue()
-        fifo.push(root)
+        fifo.push(self.root)
 
         while not fifo.isEmpty():
             node = fifo.pop()
 
             if node != None:
-                print(node.getData())
+                # print(node.getData())
 
-                if node == nodeToFind:
+                if node.getData() == data:
                     return node
                 else:
                     fifo.push(node.getLeft())
                     fifo.push(node.getRight())
 
     # depth first search
-    def search_dfs(self, root, nodeToFind):
+    # def search_dfs(self, root, data):
+    def search_dfs(self, data):
         '''
             Prochazime strom do hloubky a hledame pozadovany uzel.
             Nerekurzivni funkce za pouziti zasobniku.
         '''
         lifo = stack.Stack()
-        lifo.push(root)
+        lifo.push(self.root)
 
         while not lifo.isEmpty():
             node = lifo.pop()
 
             if node != None:
-                if node == nodeToFind:
+                if node.getData() == data:
                     return node
                 else:
                     lifo.push(node.getLeft())
                     lifo.push(node.getRight())
 
 
+    # mazani uzlu obsahujici dane data
+    def delete(self, data):
+        self.root = self.delete_data(self.root, data)
+
+        return self.root
+
     # funkce ktera nam najde nejvetsi hodnotu (tj. lezi co nejvic v pravo)
-    # v dane vetvi stromu
-    def findHighestValue(self, node):
+    # na leve strane stromu
+
+    def retrieveData(self, node):
 
         while node.getRight() is not None:
             node = node.getRight()
@@ -161,41 +184,42 @@ class BTree:
         return node.getData()
 
     # vymazeme danou hodnotu ze stromu
-    def delete(self, node, toDelete):
+    def delete_data(self, parent, toDelete):
 
-        if node == None:
-            return None
-
-        if toDelete < node.getData():
-            node.setLeft(self.delete(node.getLeft(), toDelete))
-        elif toDelete > node.getData():
-            node.setRight(self.delete(node.getRight(), toDelete))
+        if toDelete < parent.getData():
+            parent.setLeft(self.delete_data(parent.getLeft(), toDelete))
+        elif toDelete > parent.getData():
+            parent.setRight(self.delete_data(parent.getRight(), toDelete))
 
         else:
 
-            if node.getLeft() == None:
-                temp = node.getRight()
-                del node
+            if parent.getLeft() == None:
+                temp = parent.getRight()
+                del parent
                 return temp
 
-            elif node.getRight() == None:
-                temp = node.getLeft()
-                del node
+            elif parent.getRight() == None:
+                temp = parent.getLeft()
+                del parent
                 return temp
 
             else:
                 # najdeme hodnotu, ktera je co nejvetsi
                 # (tj. lezi co co nejvic v pravo) ktera je v leve vetvi
-                node.setData(self.retData(node.getLeft()))
-                # a nastavime ji jako novou hodnotu pro node (kokren vetve)
-                # + odstranime ji samozrejme z te puvodniho mista
-                node.setLeft(self.delete(node.getLeft(), node.getData()))
+                parent.setData(self.retrieveData(parent.getLeft()))
 
-        return node
+                # a nastavime ji jako novou hodnotu pro parent (kokren vetve)
+                # + odstranime ji samozrejme z te puvodniho mista
+                parent.setLeft(self.delete_data(parent.getLeft(), parent.getData()))
+
+        return parent
 
     # hezci tisk stromu
-    def print(self, root):
-        currLevel = [root]
+    def print(self):
+        currLevel = [self.root]
+
+        if self.root == None:
+            return
 
         while currLevel:
             nextLevel = list()
@@ -210,6 +234,7 @@ class BTree:
             print()
             currLevel = nextLevel
 
+
 ####################
 
 # Tree
@@ -223,35 +248,42 @@ class BTree:
 
 root = None
 tree = BTree()
-root = tree.insert(8, root)
 
-# left part of tree
-l = tree.insert(3, root)
-tree.insert(1, l)
-r = tree.insert(5, l)
-tree.insert(4, r)
+tree.insert(8)
+tree.insert(3)
+tree.insert(1)
+tree.insert(5)
+tree.insert(4)
+tree.insert(9)
+tree.insert(12)
+tree.insert(11)
 
-# right part of tree
-r = tree.insert(9, root)
-r = tree.insert(12, r)
-toFind = tree.insert(11, r)
+tree.print()
+print('-----------------')
 
 # prochazeni
-# tree.dfs_traverse_pre_order(root)
-# tree.dfs_traverse_in_order(root)
-# tree.dfs_traverse_post_order(root)
-# tree.dfs_traverse(root)
-# tree.bfs_traverse(root)
+tree.dfs_traverse_pre_order(tree.getRoot())
+# tree.dfs_traverse_in_order(tree.getRoot())
+# tree.dfs_traverse_post_order(tree.getRoot())
+
+# tree.bfs_traverse()
+# tree.dfs_traverse()
 
 # hledani
-# tree.search_bfs(root, toFind)
-# tree.search_dfs(root, toFind)
 
-tree.dfs_traverse_pre_order(root)
-root = tree.delete(root, 5)
-# root = tree.delete(root, 8)
+# node = tree.search_bfs(root, 4)
+# print(node.getData())
+
+# node = tree.search_dfs(root, 11)
+# print(node.getData())
+
+# print(tree.search_bfs(5).getData())
+# print(tree.search_dfs(5).getData())
+
+#mazani
+# root = tree.delete(5)
+# root = tree.delete(8)
 
 print('-----------------')
-# tree.dfs_traverse_pre_order(root)
+tree.print()
 
-tree.print(root)
